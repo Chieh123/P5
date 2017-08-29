c84368d41f2418fa2d3529e112b261f0dc84d8f6
Chieh123 <b04705042@ntu.edu.tw>
2017-08-29 21:55:27 +0800

    Add files via upload

--- pass9.c
+++ pass9.c
@@ -6,14 +6,18 @@
  Block 1 is super block.
  Inodes start at block 2.*/
 
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+/*#include "type.h"*/
 #include <fcntl.h>
 #include <assert.h>
+/*#include <dirent.h>*/
 #include <stdbool.h>
 #include <sys/mman.h>
 #include <string.h>
@@ -69,7 +73,7 @@ int SIZE;
 uint* used_Block;
 uint* used_Inode;
 uint* inode_link;
-struct dinode* INODE_BEGIN;
+//enum filetype{UNUSED, T_DIR, T_FILE, T_DEV};
 
 #define UNUSED 0x0
 #define T_DIR 0x1
@@ -80,8 +84,10 @@ void* get_block_address(uint block_index, void* imgPtr) {
     return (imgPtr + block_index * BSIZE);
 }
 
-struct dinode* get_inode_address(uint node_index) {
-    return &INODE_BEGIN[node_index-1];
+struct dinode* get_inode_address(uint node_index, void* imgPtr) {
+    uint block_index = 2 + node_index / IPB;
+    uint off = node_index % IPB;
+    return (struct dinode*)((imgPtr + block_index * BSIZE)) + off;
 }
 
 bool cheackValid(int index){ /*block is used or not*/
@@ -105,44 +111,17 @@ int check_bitmap() {
     int i;
     for(i = 0; i < BLOCKS; i++) {
         if(cheackValid(i)) {
-            if(used_Block[i] <= 0){
-                fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use. #%d\n",i);
-                return 1;
+            if(used_Block[i] < 0){
+                fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use.\n");
             }
         } else {
-            if(used_Block[i] > 0){
-                fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
 
-int check_inode_link(){
-    int i;
-    for(i = 1; i <= INODES; i++){
-        if(INODE_BEGIN[i].type != UNUSED && inode_link[i] < 2){
-            fprintf(stderr, "ERROR: inode marked use but not found in a directory.\n");
-            return 1;
-        }
-        if(INODE_BEGIN[i].type == T_DIR && INODE_BEGIN[i].nlink != 2){
-            fprintf(stderr, "ERROR: directory appears more than once in file system.\n");
-            return 1;
-        }
-        if(INODE_BEGIN[i].nlink != inode_link[i]-1){
-            fprintf(stderr, "ERROR: bad reference count for file.\n");
-            return 1;
-        }
-        if(INODE_BEGIN[i].type == UNUSED && INODE_BEGIN[i].nlink != 0){
-            fprintf(stderr, "ERROR: inode referred to in directory but marked free.\n");
-            return 1;
         }
     }
 }
 
 int checkRoot(struct dinode inode) {
- fprintf(stderr, "===========in checRoot========type  (%d).====\n", inode.type);
+// fprintf(stderr, "===========in checRoot========type  (%d).====\n", inode.type);
     if(inode.type != T_DIR) {
         fprintf(stderr, "ERROR: root directory does not exist.\n");
         return 1;
@@ -176,19 +155,17 @@ int checkRoot(struct dinode inode) {
 }
 
 int checkMismatch(struct dirent p, uint self) { // for parent
-
     int i, index_dir;
     struct dinode* parent;
-    parent = get_inode_address(p.inum);
+    parent = get_inode_address(p.inum % IPB, img_ptr);
     struct dirent* temp;
     int cnt = (parent->size)/ sizeof(struct dirent);
 
 //    if(cnt == 0) return 0;
-//rintf("===========in checkMismatch======cnt = %d== # = %d= type = %d=\n", cnt, p.inum, parent->type);
+
     for(i = 0; i < NDIRECT && cnt > 0; i++) {
-        temp = (struct dirent*)get_block_address(parent->addrs[i], img_ptr);
+        temp = get_block_address(parent->addrs[i], img_ptr);
         for(index_dir = 0; index_dir < BSIZE/sizeof(struct dirent) && cnt > 0; index_dir++, cnt--){
-//printf("i = %d \tindex_dir = %d \taddr = %d\n", i, index_dir, temp[index_dir].inum);
           if(temp[index_dir].inum == self) {
             return 0;
           }
@@ -197,12 +174,14 @@ int checkMismatch(struct dirent p, uint self) { // for parent
     uint* ptr = (uint*)get_block_address(parent->addrs[NDIRECT], img_ptr);
 
     for(i = 0; i < NADDRESS && cnt > 0; i++) {
-        temp = (struct dirent*)get_block_address(*(ptr + i),img_ptr);
-        for(index_dir = 0; index_dir < BSIZE/sizeof(struct dirent) && cnt > 0; index_dir++, cnt--){
-//printf("i = %d \tindex_dir = %d \taddr = %d \tname %s\n", i, index_dir, temp[index_dir].inum, temp[index_dir].name);
-             if(temp[index_dir].inum == self) {
+        temp = get_block_address(*(ptr + i), img_ptr);
+        for(i = 0; i < BSIZE / sizeof(struct dirent) && cnt > 0; i++, cnt--) {
+            temp = get_block_address(parent->addrs[i], img_ptr);
+            for(index_dir = 0; index_dir < BSIZE/sizeof(struct dirent) && cnt > 0; index_dir++, cnt--){
+                if(temp[index_dir].inum == self) {
                 return 0;
-             }
+                }
+            }
         }
     }
     fprintf(stderr, "ERROR: parent directory mismatch.\n");
@@ -215,7 +194,7 @@ int checkEntry(struct dinode inode, uint index_n) { /* for T_DIR*/
     int cnt = (inode.size)/ sizeof(struct dirent);
 //printf("inode%d size is %d", index_n, inode.size);
     if(cnt == 0) {
-        fprintf(stderr, "ERROR: directory not properly formatted.\n");
+        fprintf(stderr, "ERROR: directory not properly formatted.");
         return 1;
     }
     int i;
@@ -223,7 +202,7 @@ int checkEntry(struct dinode inode, uint index_n) { /* for T_DIR*/
         struct dirent* temp = (struct dirent*)get_block_address(inode.addrs[i],img_ptr);
         int j;
         for(j = 0; j < BSIZE / sizeof(struct dirent) && cnt >= 0; j++, cnt--) {
-//printf("i = %d \tj = %d \taddr = %d \tname %s\n", i, j, temp[j].inum, temp[j].name);
+//printf("i = %d j = %d addr = %d", i, j, temp[j].inum);
             if(strcmp(temp[j].name, ".") == 0) {
                 self = &temp[j];
             }else if(strcmp(temp[j].name,"..") == 0) {
@@ -235,11 +214,11 @@ int checkEntry(struct dinode inode, uint index_n) { /* for T_DIR*/
         }
     }
     if(parent == NULL || self == NULL) {
-        fprintf(stderr, "ERROR: directory not properly formatted.\n");
+        fprintf(stderr, "ERROR: directory not properly formatted.");
         return 1;
     }
     if(self->inum != index_n) {
-        fprintf(stderr, "ERROR: directory not properly formatted.\n");
+        fprintf(stderr, "ERROR: directory not properly formatted.");
         return 1;
     }
     if(checkMismatch(*parent, (uint)(self->inum)) == 1){
@@ -248,37 +227,8 @@ int checkEntry(struct dinode inode, uint index_n) { /* for T_DIR*/
     return 0;
 }
 
-void access_file(struct dinode inode) {
-    int i, index_dir;
-    struct dirent* temp;
-    int cnt = (inode.size)/ sizeof(struct dirent);
-
-//    if(cnt == 0) return 0;
-
-    for(i = 0; i < NDIRECT && cnt > 0; i++) {
-        temp = get_block_address(inode.addrs[i], img_ptr);
-        for(index_dir = 0; index_dir < BSIZE/sizeof(struct dirent) && cnt > 0; index_dir++, cnt--){
-            if(strcmp(temp[index_dir].name,"..") == 0)
-                continue;
-            used_Inode[temp[index_dir].inum]++;
-            inode_link[temp[index_dir].inum]++;
-        }
-    }
-    uint* ptr = (uint*)get_block_address(inode.addrs[NDIRECT], img_ptr);
-
-    for(i = 0; i < NADDRESS && cnt > 0; i++) {
-        temp = get_block_address(*(ptr + i), img_ptr);
-        for(index_dir = 0; index_dir < BSIZE/sizeof(struct dirent) && cnt > 0; index_dir++, cnt--){
-            if(strcmp(temp[index_dir].name,"..") == 0)
-                continue;
-            used_Inode[temp[index_dir].inum]++;
-            inode_link[temp[index_dir].inum]++;
-        }
-    }
-}
-
 int checkInode(struct dinode inode, uint index_b, uint index_n) {
- fprintf(stderr, "=========checkInode #%d  (%d).====================\n", index_n, inode.type);
+// fprintf(stderr, "checkInode #%d  (%d).\n", index_n, inode.type);
     if(index_n == 1){ /* todo index != 1 prarent num = self num */
         if(checkRoot(inode) == 1){
             return 1;
@@ -289,7 +239,7 @@ int checkInode(struct dinode inode, uint index_b, uint index_n) {
         return 1;
     }
 
-    int cnt = inode.size;
+//    int cnt = (inode.size) / sizeof(struct dirent);
     if(inode.type == UNUSED) {
         return 0;
     } else if(!cheackValid(index_b)) {
@@ -297,62 +247,45 @@ int checkInode(struct dinode inode, uint index_b, uint index_n) {
         return 1;
     }
     int i;
-    /*===================address valid or not===========================*/
-    for(i = 0; i < NDIRECT && cnt >= 0; i++) {
+    for(i = 0; i < NDIRECT; i++) {
         if(inode.addrs[i] <= 0) break;
-        if(cnt == 0) break;
-        if(cheackValid(inode.addrs[i])){
-            if(inode.addrs[i] > BLOCKS) {
-                fprintf(stderr, "ERROR: bad direct address in inode.\n");
-                return 1;
-            }
-        } else {
+
+//        fprintf(stderr, "inode.addrs[%d] = %d BLOCKS = %d\n", i, inode.addrs[i], BLOCKS);
+        if(cheackValid(inode.addrs[i]) && inode.addrs[i] > BLOCKS) {
+            fprintf(stderr, "ERROR: bad direct address in inode.\n");
+            return 1;
+        }
+
+        used_Block[inode.addrs[i]]++;
+    }
+
+    //fprintf(stderr, "finish direct...\n");
+
+    if(inode.addrs[NDIRECT] > 0 ){
+      if(cheackValid(inode.addrs[NDIRECT]) && inode.addrs[NDIRECT] > BLOCKS) {
+          fprintf(stderr, "ERROR: bad indirect address in inode.\n");
+          return 1;
+      }
+    }
+
+  //  fprintf(stderr, "finish indirect...\n");
+
+    used_Block[inode.addrs[NDIRECT]]++;
+
+   // fprintf(stderr, "finish checkRoot...\n");
+
+    uint* ptr = (uint*)get_block_address(inode.addrs[NDIRECT], img_ptr);
+    for(i = 0; i < NADDRESS; i++) {
+        if(*(ptr + i) <= 0) break;
+// fprintf(stderr, "inode.addrs[%d] = %d\n", inode.addrs[i]);
+        if(cheackValid(*(ptr + i)) && *(ptr + i) > BLOCKS) {
             fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
             return 1;
         }
-        cnt = cnt - BSIZE;
-//fprintf(stderr, "\nblock#%d\t",inode.addrs[i]);
-        used_Block[inode.addrs[i]]++;
+        used_Block[*(ptr + i)]++;
     }
-    if(cnt > 0 ){
-        if(cnt > 0 && !cheackValid(inode.addrs[NDIRECT])) {
-              fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
-              return 1;
-        }
-        if(cheackValid(inode.addrs[NDIRECT]) && inode.addrs[NDIRECT] > BLOCKS) {
-          fprintf(stderr, "ERROR: bad indirect address in inode.\n");
-          return 1;
-        }
-        if(used_Block[inode.addrs[NDIRECT]] != 0) {
-            fprintf(stderr, "ERROR: direct address used more than once.\n");
-            return 1; 
-        }
-//fprintf(stderr, "\nblock#%d\t",inode.addrs[NDIRECT]);
-        used_Block[inode.addrs[NDIRECT]]++;
-        uint* ptr = (uint*)get_block_address(inode.addrs[NDIRECT], img_ptr);
-        for(i = 0; i < NADDRESS && cnt > 0; i++) {
-           if(*(ptr + i) <= 0) break;
-           if(cheackValid(*(ptr + i))) {
-               if(*(ptr + i) > BLOCKS) {
-                    fprintf(stderr, "ERROR: bad indirect address in inode.\n");
-                    return 1;
-               }
-           }else {
-               fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
-               return 1;
-           }
-           if(used_Block[*(ptr + i)] != 0) {
-               fprintf(stderr, "ERROR: indirect address used more than once.\n");
-               return 1; 
-           }
-           cnt = cnt - BSIZE;
-//fprintf(stderr, "\nblock#%d\t",*(ptr + i));
-           used_Block[*(ptr+ i)]++;
-         }
-     }
-    /*===================over address valid or not===========================*/
 
-
+   // fprintf(stderr, "finish used_Block...\n");
 
     if(inode.type == T_DIR) {
         if(checkEntry(inode, index_n) == 1)
@@ -361,7 +294,6 @@ int checkInode(struct dinode inode, uint index_b, uint index_n) {
             fprintf(stderr, "ERROR: directory appears more than once in file system.\n");
             return 1;
         }
-        access_file(inode);
         used_Inode[index_n]++;
     } else if(inode.type == T_FILE) {
         used_Inode[index_n]++;
@@ -405,8 +337,8 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "SIZE %d\n", SIZE); */
 
     used_Block = (uint*)malloc(sizeof(uint) * BLOCKS);
-    used_Inode = (uint*)malloc(sizeof(uint) * INODES + 1);
-    inode_link = (uint*)malloc(sizeof(uint) * INODES + 1);
+    used_Inode = (uint*)malloc(sizeof(uint) * INODES);
+    inode_link = (uint*)malloc(sizeof(uint) * INODES);
 
 
 
@@ -414,41 +346,39 @@ int main(int argc, char *argv[]) {
     for(i = 0; i < BLOCKS; i++){
         used_Block[i] = 0;
     }
-    for(i = 0; i < INODES + 1; i++){
+    for(i = 0; i < INODES; i++){
         used_Inode[i] = 0;
         inode_link[i] = 0;
     }
-//fprintf(stderr, "block#%d\n\n",block_index);
-    used_Block[0]++;
     used_Block[block_index]++;
     block_index = block_index +1;
+
     struct dinode* inode;
 /*    strncpy(bitmap, (const char *)get_block_address(block_index + INODES, img_ptr), BSIZE);*/
     bitmap = (char*)get_block_address(block_index + INODES/IPB + 1, img_ptr);//INODES
-    used_Block[block_index + INODES/IPB + 1]++;
-    used_Block[block_index + INODES/IPB]++;
+
 
 
 // fprintf(stderr, "======img_ptr %x==inode blocks % INODES/IPB %d ==bitmap %x=====block_index:%d===========\n",
 //    img_ptr,  INODES/IPB, (block_index + INODES/IPB +  1) * BSIZE, block_index);
 
 
-    INODE_BEGIN = ((struct dinode*)get_block_address(block_index, img_ptr)) + i%IPB;
     int j = 0;
     for(i = 0; i < INODES; i++){
         inode = ((struct dinode*)get_block_address(block_index, img_ptr)) + i%IPB;
         if(checkInode(*inode, block_index, i) == 1) {
             exit(1);
         }
-//fprintf(stderr, "block#%d\n\n",block_index);
         used_Block[block_index]++;
         if(i/IPB > j) {
             block_index = block_index +1;
             j++;
         }
     }
-    if(check_bitmap() == 1) {
-        exit(1);
+
+    for(i = 0; i < INODES; i++){
+        inode = (struct dinode*)get_block_address(block_index, img_ptr);
+
     }
     exit(0);
 }
